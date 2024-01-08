97e7fc5201590c63b9f105d975e290f4ee8cce85
SUJI4812 <happy4812345@gmail.com>
2024-01-08 16:15:07 +0900

    hsj

--- config/settings.py
+++ config/settings.py
@@ -74,17 +74,24 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "book_db",
         "HOST" : "localhost",
         "PORT" : "3306",
         "USER" : "maria",
-        "PASSWORD": "1234",              
+        "PASSWORD": "1234", """              
+""" }
+
+} """
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite",
+}
 }
 
-}
 
 
 # Password validation
